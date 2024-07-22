2a738c813afd89192a77f57d61382232fa5a3b11
NamK666 <chriscole77@naver.com>
2024-07-22 15:09:43 +0900

    Feat: initial commit

---
+++ src/main/java/com/example/fashionlog/Domain/Domain.java
@@ -0,0 +1,5 @@
+package com.example.fashionlog.Domain;
+
+public class Domain {
+	/* dfdfdfdf */
+}
---
+++ src/main/java/com/example/fashionlog/Service/Service.java
@@ -0,0 +1,5 @@
+package com.example.fashionlog.Service;
+
+public class Service {
+	/* dfdfdfdf */
+}
