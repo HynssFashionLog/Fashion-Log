5a6007f1a9ef40195e67228fed591d5725f43152
LLSNsssz <seungjang93@gmail.com>
2024-08-02 09:47:14 +0900

    Feat: 회원 엔티티 클래스 구현
    - 회원 정보 필드 정의 (ID, 이름, 닉네임, 전화번호, 이메일, 비밀번호 등)
    - 생성, 수정, 삭제 시간 필드 추가
    - JPA 엔티티 매핑 어노테이션 적용
    - Lombok 어노테이션을 통한 보일러플레이트 코드 제거
    - Builder 패턴 적용으로 객체 생성 편의성 향상

--- src/main/java/com/example/fashionlog/domain/Member.java
+++ src/main/java/com/example/fashionlog/domain/Member.java
@@ -8,19 +8,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "member")
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Builder
 public class Member {
 
 	@Id
